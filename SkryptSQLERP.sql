4f08fc63dd198691f0ad762f631046608a98ff98
Dominik <Dominik71830@gmail.com>
2016-04-06 19:04:32 +0200

    Poprawki do powiadamiania

--- SkryptSQLERP.sql
+++ SkryptSQLERP.sql
@@ -132,6 +132,8 @@ INSERT INTO positions (id,name) VALUES (4,'Technik');
 
 INSERT INTO emails (id,id_sender,id_receiver,text,date,checked) VALUES (1,1,2,'srutututu pęczek drutu','2010-01-01 17:04:18',false);
 INSERT INTO emails (id,id_sender,id_receiver,text,date,checked) VALUES (2,4,1,'Siała baba mak','2010-01-01 17:04:18',false);
+INSERT INTO emails (id,id_sender,id_receiver,text,date,checked) VALUES (3,2,1,'srutututu pęczek drutu','2010-01-01 17:04:18',false);
+
 
 INSERT INTO repairs (id,id_employee,client_name,client_full_name,description,date,executed) VALUES (1,1,'Antek','Franczak','Coś sie popsuło i niedziała','2010-01-01 17:04:18',false);
 INSERT INTO repairs (id,id_employee,client_name,client_full_name,description,date,executed) VALUES (2,1,'Jasio','Nowakowski','Trzeszczy coś','2010-01-01 17:04:18',true);
